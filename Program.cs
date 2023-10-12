44b614ca474c6ca2d7677c758523471f2e88c1d9
Tigran <134860695+tigranharutunyan@users.noreply.github.com>
2023-10-12 20:13:28 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 23, 24, 25, 26, 36, 9,17, 8, 4, 1, 2, 6, 3 };
+            for(int i = 0;i < number.Length; i++) {
+
+                for (int j = 4; j < 10; j++)
+                {
+                    if (number[i] % j == 0 )
+                    {
+                        Console.WriteLine(number[i]);
+                        break;
+                    }
+                  
+                }
+                
+            }
+        }
+    }
+}
